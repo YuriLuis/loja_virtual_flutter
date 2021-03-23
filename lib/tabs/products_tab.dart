068d95b18f9397d1888acd124ed7cac2e4a18059
yuri.pereira <yuri.pereira@ambevtech.com.br>
2021-03-23 12:37:14 -0300

    Aula 141 monstrando lista de categorias.

--- lib/tabs/products_tab.dart
+++ lib/tabs/products_tab.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtual_flutter/tiles/category_tile.dart';
 
 class ProductsTab extends StatelessWidget {
   @override
@@ -7,15 +8,17 @@ class ProductsTab extends StatelessWidget {
     return FutureBuilder<QuerySnapshot>(
       future: Firestore.instance.collection("products").getDocuments(),
       // ignore: missing_return
-      builder: (context,snapshot){
-        if(!snapshot.hasData){
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
           return Center(
-            child: CircularProgressIndicator(),);
-        }else {
+            child: CircularProgressIndicator(),
+          );
+        } else {
+          var dividerdTiles = ListTile.divideTiles(
+                  tiles: snapshot.data.documents.map((e) => CategoryTile(e)).toList(),
+          color: Colors.grey).toList();
           return ListView(
-            children: [
-
-            ],
+            children: dividerdTiles
           );
         }
       },
