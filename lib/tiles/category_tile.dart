7cd5b9b8efac934868b827cee74f775ba8cf9a96
yuri.pereira <yuri.pereira@ambevtech.com.br>
2021-03-26 11:28:24 -0300

    Aula 144 Criando tela de produtos

--- lib/tiles/category_tile.dart
+++ lib/tiles/category_tile.dart
@@ -1,12 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtual_flutter/screens/category_screen.dart';
 
 class CategoryTile extends StatelessWidget {
-
   final DocumentSnapshot snapshot;
 
   CategoryTile(this.snapshot);
-
 
   @override
   Widget build(BuildContext context) {
@@ -18,8 +17,9 @@ class CategoryTile extends StatelessWidget {
       ),
       title: Text(snapshot.data['title']),
       trailing: Icon(Icons.keyboard_arrow_right),
-      onTap: (){
-
+      onTap: () {
+        Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => CategoryScreen(snapshot)));
       },
     );
   }
