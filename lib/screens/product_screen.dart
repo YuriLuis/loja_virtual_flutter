f3d0ad6216f6d80b6c05b9f928d1519ace4f8bda
yuri.pereira <yuri.pereira@ambevtech.com.br>
2021-03-26 12:37:41 -0300

    Aula 147 criando tile da lista

---
+++ lib/screens/product_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual_flutter/datas/product_data.dart';
+
+class ProductScreen extends StatefulWidget {
+
+  final ProductData product;
+  ProductScreen(this.product);
+
+  @override
+  _ProductScreenState createState() => _ProductScreenState(this.product);
+}
+
+class _ProductScreenState extends State<ProductScreen> {
+  final ProductData product;
+  _ProductScreenState(this.product)
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
