2206bb6060e1690b377dfc65d28d39ca934d4f17
yuri.pereira <yuri.pereira@ambevtech.com.br>
2021-04-06 15:05:28 -0300

    Aula 176 Mostrando os detalhes do pedido

--- lib/tiles/order_tile.dart
+++ lib/tiles/order_tile.dart
@@ -1,10 +1,13 @@
+import 'dart:collection';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class OrderTile extends StatelessWidget {
-
   final String orderId;
+
   OrderTile(this.orderId);
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -12,17 +15,26 @@ class OrderTile extends StatelessWidget {
       child: Padding(
         padding: EdgeInsets.all(8.0),
         child: StreamBuilder<DocumentSnapshot>(
-          stream: Firestore.instance.collection('orders').document(orderId).snapshots(),
+          stream: Firestore.instance
+              .collection('orders')
+              .document(orderId)
+              .snapshots(),
           // ignore: missing_return
-          builder: (context,snapshot){
-            if(!snapshot.hasData){
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
               return Center(
                 child: CircularProgressIndicator(),
               );
-            }else {
+            } else {
               return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('Código do pedido: ${snapshot.data.documentID}', style: TextStyle(fontWeight: FontWeight.bold),)
+                  Text(
+                    'Código do pedido: ${snapshot.data.documentID}',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 4.0,),
+                  Text(_buildProductsText(snapshot.data))
                 ],
               );
             }
@@ -30,5 +42,15 @@ class OrderTile extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  // ignore: missing_return
+  String _buildProductsText(DocumentSnapshot snapshot) {
+    String text = "Descrição:\n";
+    for(LinkedHashMap p in snapshot.data['products']){
+      text += '${p['quantity']} x ${p['product']['title']} (R\$ ${p['product']['price'].toStringAsFixed(2)})\n';
+    }
+    text += 'Total: R\$ ${snapshot.data['total'].toStringAsFixed(2)}';
+    return text;
   }
 }
